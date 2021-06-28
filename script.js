57112a8a5b95632986b6a48eeace74507e2ae168
Gepoverlow <gepeoverload@gmail.com>
2021-06-28 19:52:28 +0200

    rps game finished

--- script.js
+++ script.js
@@ -88,77 +88,141 @@ if (playerScore > computerScore) {
 
 */
 
-const computerPlay = function () {
-  const randomC = Math.floor(Math.random() * 3) + 1;
-  let cWeapon = "";
-  if (randomC === 1) {
-    cWeapon = "rock";
-    return cWeapon;
-  } else if (randomC === 2) {
-    cWeapon = "paper";
-    return cWeapon;
-  } else if (randomC === 3) {
-    cWeapon = "scissors";
-    return cWeapon;
-  }
-};
-let playerSelection = "";
-let computerSelection = computerPlay();
-let playerScore = 0;
-let computerScore = 0;
-
-// console.log(playerSelection, computerSelection);
 const results = document.querySelector(".results");
-
-const playRound = function (playerSelection, computerSelection) {
-  if (playerSelection === "rock" && computerSelection === "rock") {
-    results.textContent = "Its a Draw!";
-  } else if (playerSelection === "rock" && computerSelection === "paper") {
-    results.textContent = "Computer Wins ! Paper beats Rock";
-    return (computerScore = +computerScore + 1);
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    results.textContent = "Player Wins! Rock beats Scissors";
-    return (playerScore = playerScore + 1);
-  } else if (playerSelection === "paper" && computerSelection === "paper") {
-    results.textContent = "Its a Draw!";
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    results.textContent = "Computer Wins ! Scissors beats Paper";
-    return (computerScore = +computerScore + 1);
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    results.textContent = "Player Wins! Paper beats Rock";
-    return (playerScore = playerScore + 1);
-  } else if (
-    playerSelection === "scissors" &&
-    computerSelection === "scissors"
-  ) {
-    results.textContent = "Its a Draw!";
-  } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    results.textContent = "Computer Wins ! Rock beats Scissors";
-    return (computerScore = +computerScore + 1);
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    results.textContent = "Player Wins! Scissors beats Paper";
-    return (playerScore = playerScore + 1);
-  }
-};
-
 const btnRock = document.querySelector(".rock");
 const btnPaper = document.querySelector(".paper");
 const btnScissor = document.querySelector(".scissor");
+const plyrScore = document.querySelector(".numberPlayer");
+const compScore = document.querySelector(".numberComputer");
+const btns = document.querySelector(".btns");
+const restart = document.querySelector(".rbtn");
+// const win = document.querySelector(".win");
 
-btnRock.addEventListener("click", function () {
-  playerSelection = "rock";
-  playRound(playerSelection, computerPlay());
-});
-btnPaper.addEventListener("click", function () {
-  playerSelection = "paper";
-  playRound(playerSelection, computerPlay());
-});
-btnScissor.addEventListener("click", function () {
-  playerSelection = "scissors";
-  playRound(playerSelection, computerPlay());
-});
+const game = function () {
+  ////////////// COMPUTER PLAY ////////////////////////
 
-// let testing = 2 + 2;
+  const computerPlay = function () {
+    const randomC = Math.floor(Math.random() * 3) + 1;
+    let cWeapon = "";
+    if (randomC === 1) {
+      cWeapon = "rock";
+      return cWeapon;
+    } else if (randomC === 2) {
+      cWeapon = "paper";
+      return cWeapon;
+    } else if (randomC === 3) {
+      cWeapon = "scissors";
+      return cWeapon;
+    }
+  };
+  let playerSelection = "";
+  let computerSelection = computerPlay();
+  let playerScore = 0;
+  let computerScore = 0;
 
-// const score = document.querySelector(".score");
-// score.textContent = `hey here is the score ${testing}`;
+  ////////////// PLAYING ROUND////////////////////////
+  const playRound = function (playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "rock") {
+      results.textContent = `Computer selects ${computerSelection.toUpperCase()} . Its a Draw!`;
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+      results.textContent = `Computer selects ${computerSelection.toUpperCase()} . Computer Wins ! Paper beats Rock`;
+      return (computerScore = +computerScore + 1);
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+      results.textContent = `Computer selects ${computerSelection.toUpperCase()} . Player Wins! Rock beats Scissors`;
+      return (playerScore = playerScore + 1);
+    } else if (playerSelection === "paper" && computerSelection === "paper") {
+      results.textContent = `Computer selects ${computerSelection.toUpperCase()} . Its a Draw!`;
+    } else if (
+      playerSelection === "paper" &&
+      computerSelection === "scissors"
+    ) {
+      results.textContent = `Computer selects ${computerSelection.toUpperCase()} . Computer Wins ! Scissors beats Paper`;
+      return (computerScore = +computerScore + 1);
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+      results.textContent = `Computer selects ${computerSelection.toUpperCase()} . Player Wins! Paper beats Rock`;
+      return (playerScore = playerScore + 1);
+    } else if (
+      playerSelection === "scissors" &&
+      computerSelection === "scissors"
+    ) {
+      results.textContent = `Computer selects ${computerSelection.toUpperCase()} . Its a Draw!`;
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+      results.textContent = `Computer selects ${computerSelection.toUpperCase()} . Computer Wins ! Rock beats Scissors`;
+      return (computerScore = +computerScore + 1);
+    } else if (
+      playerSelection === "scissors" &&
+      computerSelection === "paper"
+    ) {
+      results.textContent = `Computer selects ${computerSelection.toUpperCase()} . Player Wins! Scissors beats Paper`;
+      return (playerScore = playerScore + 1);
+    }
+  };
+
+  ////////////// EVENT LISTENERS ////////////////////////
+
+  restart.addEventListener("click", function () {
+    btns.classList.remove("hidden");
+    results.classList.remove("win");
+    results.textContent = "Pick your weapon";
+    playerScore = 0;
+    computerScore = 0;
+    plyrScore.textContent = playerScore;
+    compScore.textContent = computerScore;
+  });
+
+  btnRock.addEventListener("click", function () {
+    playerSelection = "rock";
+    computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    plyrScore.textContent = `${playerScore}`;
+    compScore.textContent = `${computerScore}`;
+    if (playerScore === 5) {
+      results.textContent = "Player Wins!";
+      btns.classList.add("hidden");
+      results.classList.add("win");
+    } else if (computerScore === 5) {
+      results.textContent = "Computer Wins!";
+      btns.classList.add("hidden");
+      results.classList.add("win");
+    }
+  });
+  btnPaper.addEventListener("click", function () {
+    playerSelection = "paper";
+    computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    plyrScore.textContent = `${playerScore}`;
+    compScore.textContent = `${computerScore}`;
+    if (playerScore === 5) {
+      results.textContent = "Player Wins!";
+      btns.classList.add("hidden");
+      results.classList.add("win");
+    } else if (computerScore === 5) {
+      results.textContent = "Computer Wins!";
+      btns.classList.add("hidden");
+      results.classList.add("win");
+    }
+  });
+  btnScissor.addEventListener("click", function () {
+    playerSelection = "scissors";
+    computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    plyrScore.textContent = `${playerScore}`;
+    compScore.textContent = `${computerScore}`;
+    if (playerScore === 5) {
+      results.textContent = "Player Wins!";
+      btns.classList.add("hidden");
+      results.classList.add("win");
+    } else if (computerScore === 5) {
+      results.textContent = "Computer Wins!";
+      btns.classList.add("hidden");
+      results.classList.add("win");
+    }
+  });
+};
+
+game();
+
+// const plyrScore = document.querySelector(".numberPlayer");
+// const compScore = document.querySelector(".numberComputer");
+// plyrScore.textContent = `${playerScore}`;
+// compScore.textContent = `${computerScore}`;
